f183e0584a28525dd0d8a9471a721e4373c489e7
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-11-29 21:40:59 -0300

    Alterando o tsconfig para não adicionar tipo Any e informando tipos corretos nas classes Negciacao e NegociacaoController

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,7 +1,9 @@
+import { Negociacao } from "../models/negociacao.js";
+
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+    private inputData : HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
 
     constructor(){
         this.inputData = document.querySelector('#data');
@@ -9,9 +11,16 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+
+    criaNegociacao () : Negociacao{
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp,','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(data, quantidade, valor);
     }
 }
