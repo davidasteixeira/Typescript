6975fe7b7b030889dabe897df2f51572b3910d74
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-12-10 20:35:37 -0300

    Restringindo negociações apenas dias úteis e criando Enum para dias da semana
    Enum criada para validar junto ao método ehDiaUtil e poder ser utilizada em toda a aplicação.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -18,16 +19,25 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void {
+    public adiciona(): void {
         const negociacao = this.criaNegociacao();
+        
+        if(!this.ehDiaUtil(negociacao.data)){
+           this.mensagemView.update('Apenas negociações em dias úteis são aceitas.');
+           return;
+        }
+
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso')
-        console.log(this.negociacoes.lista());
         this.limparFormulario();
+        this.atualizaView();
+    }
+    
+    private ehDiaUtil(data:Date){
+        return data.getDay() > DiasDaSemana.DOMINGO
+         && data.getDay() < DiasDaSemana.SABADO;
     }
 
-    criaNegociacao () : Negociacao{
+    private criaNegociacao () : Negociacao{
         const exp = /-/g;
         const data = new Date(this.inputData.value.replace(exp,','));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -35,10 +45,15 @@ export class NegociacaoController {
         return new Negociacao(data, quantidade, valor);
     }
 
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+
+    private atualizaView():void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso')
     }
 }
