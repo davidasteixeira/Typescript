ecc11dc7a66876328e5f03a8812a6262728abb73
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2023-01-16 21:56:16 -0300

    alterando arvores de pastas do projeto e adicionando decorator de tempo de execução

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
+
 export abstract class View<T> {
     protected elemento: HTMLElement;
     private escapar = false;
@@ -13,7 +15,7 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-
+    @logarTempoDeExecucao()
     public update(model:T):void {
         let template = this.template(model);
         if(this.escapar){
